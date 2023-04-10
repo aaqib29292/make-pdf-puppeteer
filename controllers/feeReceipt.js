d68e8e32a6360b501d369ff7af16aa9944df6909
Aaqib Parvez <aaqib29292@gmail.com>
2023-04-10 11:41:43 +0530

    added vercel json

--- controllers/feeReceipt.js
+++ controllers/feeReceipt.js
@@ -1,4 +1,5 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
+const chromium = require("chrome-aws-lambda");
 
 // (async () => {
 //   const domain = 'https://finschoolb.dev.newtonclassroom.com'
@@ -28,10 +29,17 @@ async function generateFeeReceipt(req, res) {
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaXNzaW9uT2ZmaWNlciIsImVtYWlsIjoicHJvLmFkbUBnZWR1LmRlbW8uc3VycmVhbC5jb21wYW55Iiwic2Nob29sSWQiOjE3LCJzY2hvb2xDb2RlIjoiZmluc2Nob29sYiIsImlhdCI6MTY3NjI4Njk0Mn0.5ZAzo7p7jnrxSHzDawub5tcmBC4waExoJLhXueM8wDo'
     // const domain = 'http://finschoola.localhost:3000'
     // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4iLCJlbWFpbCI6ImNoYW5jZWxvci5hZG1AZ2VkdS5kZW1vLnN1cnJlYWwuY29tcGFueSIsInNjaG9vbElkIjoxNiwic2Nob29sQ29kZSI6ImZpbnNjaG9vbGEiLCJpYXQiOjE2ODA3ODc1Nzh9.3FGkFDBu0Uy8t26HOC1TDlt-aGQ7GxrmbAqLtmcrAO8'
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
+    // const browser = await puppeteer.launch({
+    //   headless: true,
+    //   args: ['--no-sandbox', '--disable-setuid-sandbox']
+    // });
+  const browser = await puppeteer.launch({
+    args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
+  })
     const page = await browser.newPage();
     await page.goto(`${domain}/login?status=success&jwt=${token}&role=admin`, {
       waitUntil: 'networkidle2',
@@ -42,7 +50,6 @@ async function generateFeeReceipt(req, res) {
     // page.pdf() is currently supported only in headless mode.
     // @see https://bugs.chromium.org/p/chromium/issues/detail?id=753118
     const buffer = await page.pdf({
-      path: 'hn.pdf',
       format: 'A4',
     });
 
--- index.js
+++ index.js
@@ -4,21 +4,24 @@ const express = require('express')
 const corsHeader = require('./middlewares/cors')
 
 const pdfRoute = require('./routes/feeReciptPDF')
-
+const cors = require("cors")
 const app = express();
 
 app.options('*', corsHeader )
 
 app.use(corsHeader)
 
+app.use(cors())
+
 app.use(express.json())
 
-app.use('/api/pdf', pdfRoute)
-
-const PORT = process.env.PORT || 8081;
+app.use('/', pdfRoute)
+// app.get("/", (req, res) => {
+//   res.send("Main page!");
+// });
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log('server is running on port -', PORT))
 
-module.export = app
 
 
